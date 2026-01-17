455881a528904131a266fc39d254e900ac5969d3
Peneko113 <103440308+Peneko113@users.noreply.github.com>
2026-01-17 12:03:45 +0800

    启用禁用员工账号业务代码开发

--- back-end/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ back-end/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -111,4 +111,20 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new PageResult(total,results);
     }
 
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    public void startOrStop(Integer status, Long id) {
+        Employee employee = Employee.builder()
+                .status(status)
+                .id(id)
+                .updateTime(LocalDateTime.now())
+                .updateUser(BaseContext.getCurrentId())
+                .build();
+
+        employeeMapper.update(employee);
+    }
+
 }
