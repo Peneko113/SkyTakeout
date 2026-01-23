a7344fe89bf5dfbd50d86969419b9cb6b5817e36
Peneko113 <103440308+Peneko113@users.noreply.github.com>
2026-01-24 00:20:40 +0800

    删除菜品业务代码开发

--- back-end/sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ back-end/sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -8,11 +8,10 @@ import com.sky.service.DishService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -47,5 +46,17 @@ public class DishController {
         log.info("分页查询菜品:{}", dishPageQueryDTO);
         PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    /**
+     * 菜品批量删除
+     * @return
+     */
+    @DeleteMapping
+    @ApiOperation("菜品批量删除")
+    public Result delete(@RequestParam List<Long> ids) {
+        log.info("菜品批量删除:{}", ids);
+        dishService.deleteBatch(ids);
+        return Result.success();
     }
 }
